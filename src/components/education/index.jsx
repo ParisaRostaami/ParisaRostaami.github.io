88f4cb6f410da9b75e11703c83e8324654904746
Parisa Rostami <69730582+ParisaRostaami@users.noreply.github.com>
2023-08-31 09:41:00 +0330

    index.jsx

--- src/components/education/index.jsx
+++ src/components/education/index.jsx
@@ -41,7 +41,6 @@ const Education = ({ loading, education }) => {
   return (
     <>
       {education?.length !== 0 && (
-      <div className="flex justify-center items-center h-screen">
         <div className="card shadow-lg compact bg-base-100">
           <div className="card-body">
             <div className="mx-3">
@@ -75,7 +74,6 @@ const Education = ({ loading, education }) => {
             </div>
           </div>
         </div>
-     </div>
 
       )}
     </>
